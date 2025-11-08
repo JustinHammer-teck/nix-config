cd58c04bfea0688f0ead49e406135ae174bc577c
Moritz Zimmermann <81556222+JustinHammer-teck@users.noreply.github.com>
2025-11-08 23:09:40 +1100

    [New] add configurate for popcorn host

---
+++ host/popcorn/configuration.nix
@@ -0,0 +1,212 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/nixos/virtualisation
+  ];
+
+  services.virtualisation.podman.enable = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Security
+  security.pam.sshAgentAuth.enable = true;
+
+  # Pick only one of the below networking options.
+  # networking = {
+  #   hostName = "chabo";
+  #   interfaces.wlp3s0 = {
+  #     useDHCP = true;
+  #     ipv4.addresses = [
+  #       {
+  #         address = "192.168.1.103";
+  #         prefixLength = 24;
+  #       }
+  #     ];
+  #   };
+  #   wireless = {
+  #     enable = true;
+  #     networks = {
+  #       TelstraD3ADC8 = {
+  #         pskRaw = "7df08cb5841804118bcbe9ee72b5fff983c6f7ba533b47e7ce363258273283f5";
+  #       };
+  #     };
+  #   };
+  # }; # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+  networking.firewall = {
+    enable = false;
+
+    #allow traffic from tailscale
+    trustedInterfaces = [
+      "tailscale0"
+      "enp2s0"
+    ];
+
+    allowedTCPPorts = [
+      22
+    ];
+
+    allowedUDPPorts = [ config.services.tailscale.port ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Australia/Sydney";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.moritzzmn = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILqP1HvcppNVOVZn/B3hd6He1ibPsTisvL16su7k9/7k moritzzmn@imbp"
+    ];
+  };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    allowSFTP = false;
+    ports = [ 22 ];
+
+    settings = {
+      LogLevel = "INFO";
+      AllowUsers = [ "moritzzmn" ];
+      PasswordAuthentication = true;
+      X11Forwarding = false;
+      KbdInteractiveAuthentication = true;
+      PermitRootLogin = "no";
+    };
+
+    extraConfig = ''
+      PubkeyAuthentication yes
+
+      ClientAliveCountMax 0
+      ClientAliveInterval 300
+
+      AllowTcpForwarding no
+      AllowAgentForwarding no
+      MaxAuthTries 3
+      MaxSessions 2
+      TCPKeepAlive yes
+    '';
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  services.logind.extraConfig = ''
+    KillUserProcesses=no
+  '';
+
+  # List services that you want to enable:
+  # Flake support enable
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 15d";
+    };
+    optimise.automatic = true;
+  };
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ host/popcorn/default.nix
@@ -0,0 +1,61 @@
+{
+  self,
+  inputs,
+  nixpkgs,
+  nixpkgs-unstable,
+  lib,
+  ...
+}:
+let
+  popcorn = {
+    user = "moritzzmn";
+    host = "popcorn";
+    editor = "nvim";
+    platform = "x86_64-linux";
+  };
+
+  system = "${popcorn.platform}";
+
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+
+  pkgs-unstable = import nixpkgs-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in
+nixpkgs.lib.nixosSystem {
+  inherit system;
+  specialArgs = {
+    inherit
+      inputs
+      self
+      pkgs
+      pkgs-unstable
+      lib
+      popcorn
+      ;
+  };
+  modules = [
+    ./configuration.nix
+    ./hardware-configuration.nix
+    inputs.omarchy-nix.nixosModules.default
+    inputs.determinate.nixosModules.default
+    inputs.home-manager.nixosModules.home-manager
+    {
+      omarchy = {
+        full_name = "Moritz Zimmerman";
+        email_address = "dinhnhattai.nguyen@hotmail.com";
+        theme = "everforest";
+      };
+
+      home-manager = {
+        users.your-username = {
+          imports = [ inputs.omarchy-nix.homeManagerModules.default ];
+        };
+      };
+    }
+  ];
+}
